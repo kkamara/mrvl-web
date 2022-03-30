49f466be61ab18a0efc39c08127b656bfa44c447
kkamara <kelvinkamara@protonmail.com>
2022-03-30 12:55:43 +0100

    Minor ui updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,17 @@
-import React from "react"
-import { APP_NAME, YEAR_RELEASED, } from "../constants"
+import React from 'react'
+import { APP_NAME, YEAR_RELEASED, } from '../constants'
+
+import './Footer.scss'
 
 const Footer = () => (
-    <footer className="footer" style={styles.footer}>
-        <div className="container text-center">
-            <span className="text-muted">
-                {APP_NAME} &copy; {YEAR_RELEASED}&nbsp;
-                <a target="_blank" href="https://github.com/kkamara/mrvl">
-                    Github
-                </a>
-            </span>
+    <footer className='footer'>
+        <div className='container text-center footer-text'>
+            {APP_NAME} &copy; {YEAR_RELEASED}&nbsp;
+            <a target='_blank' href='https://github.com/kkamara/mrvl'>
+                Github
+            </a>
         </div>
     </footer>
 )
-
-const styles = {
-    footer: {
-        marginTop: 30
-    }
-}
 
 export default Footer
