8be9bc511c907d35010bbbfd67967921fb3e2fa6
kkamara <kelvinkamara@protonmail.com>
2022-10-30 09:04:54 +0000

    Improve speed with lazy loading code-splitting

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,28 @@
-import { BrowserRouter, Switch, Redirect, Route } from 'react-router-dom'
-import React, { Fragment } from 'react'
+import { BrowserRouter, Switch, Redirect, Route, } from 'react-router-dom'
+import React, { Fragment, lazy, Suspense, } from 'react'
 
 import $ from'jquery'
 import Popper from'popper.js'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min'
 
-import FavouriteComicsPage from './components/FavouriteComics/FavouriteComicsPage'
-import HomePage from './components/Comics/HomePage'
-import ComicPage from './components/Comics/ComicPage'
-import SearchComicsPage from './components/Comics/SearchComicsPage'
-import Page404 from './components/Page404'
-import Footer from './components/Footer'
-import Header from './components/Header'
-
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
+
+import Header from './components/Header'
+import Loader from "./components/Loader"
+
+const FavouriteComicsPage = lazy(() => import('./components/FavouriteComics/FavouriteComicsPage'))
+const HomePage = lazy(() => import('./components/Comics/HomePage'))
+const ComicPage = lazy(() => import('./components/Comics/ComicPage'))
+const SearchComicsPage = lazy(() => import('./components/Comics/SearchComicsPage'))
+const Page404 = lazy(() => import('./components/Page404'))
+const Footer = lazy(() => import('./components/Footer'))
 
 const App = () => (
     <div id='app'>
         <BrowserRouter>
+          <Suspense fallback={Loader}>
             <Fragment>
                 <Header />
                 <Switch>
@@ -32,6 +35,7 @@ const App = () => (
                 </Switch>
                 <Footer />
             </Fragment>
+          </Suspense>
         </BrowserRouter>
     </div>
 )
